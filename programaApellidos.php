63cb24096a233b8d6899e0bfee7b7d48b6bf767a
carloswildcanon <chiquito_wc@hotmail.com>
2023-11-13 06:21:32 -0300

    Se ingresa el menu

--- programaApellidos.php
+++ programaApellidos.php
@@ -31,6 +31,28 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
+/**
+ * menu de inicio
+ * @return int
+ */
+function seleccionarOpcion(){
+    //int $opcionElegida
+    do{
+        echo "-------------------------------------------  \n";
+        echo "1) Jugar Wordix con una palabra elegida.\n";
+        echo "2) Jugar Wordix con una palabra aleatoria.\n";
+        echo "3) Mostrar una partida.\n";
+        echo "4) Mostrar la primer partida.\n";
+        echo "5) Mostrar resumen del jugador.\n";
+        echo "6) Mostrar listado de partidas ordenadas por jugador y por palabra.\n";
+        echo "7) Agregar una palabra de 5 letras a Wordix.\n";
+        echo "8) Salir.\n";
+        echo "Selecione una de las opciones anteriores:(un numero de 1-8) ";
+        $opcionElegida = trim(fgets(STDIN));
+    }while(!(is_numeric($opcionElegida) && (($opcionElegida == (int)$opcionElegida) && ($opcionElegida >= 1 &&  $opcionElegida<= 8))));    
+    return $opcionElegida;
+}
+
 /* ****COMPLETAR***** */
 
 
